56512042b7bc8e2f0e442c1b9b9410ca257e4986
liFlowStone <lizhe1563@163.com>
2020-03-06 01:10:31 +0800

    3月5日学习了springboot的日志配置

--- src/test/java/com/lrs/UseSpringBootTest.java
+++ src/test/java/com/lrs/UseSpringBootTest.java
@@ -4,6 +4,8 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -26,7 +28,8 @@ public class UseSpringBootTest {
 	Person person;
 	@Autowired
 	ApplicationContext ioc;
-	
+	//记录器
+	Logger logger = LoggerFactory.getLogger(getClass());
 	@Test
 	public void helloService() {
 		System.out.println(ioc.containsBean("helloService"));
@@ -34,5 +37,17 @@ public class UseSpringBootTest {
 	@Test
 	public void contextLoads() {
 		System.out.println(person);
+	}
+	@Test
+	public void loggerTest() {
+		//日志的级别
+		//由低到高
+		//可以调整日志的输出级别，日志只会在这个级别的高级别生效
+		logger.trace("这是trace日志。。。");
+		logger.debug("这是debug日志。。。");
+		//springboot默认使用的是info级别，没有指定级别的就用springboot默认级别：root级别
+		logger.info("这是info日志。。。");
+		logger.warn("这是warn日志。。。");
+		logger.error("这是error日志。。。");
 	}
 }
